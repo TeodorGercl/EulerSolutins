0d9ec2dfabc6b8c033cb6fdcc67feefe55ba5e3e
Sergei Novikov <snovikov@305-system.nonstop.loc>
2017-01-17 12:57:17 +0500

    PEP8 fix

--- 004/004.py
+++ 004/004.py
@@ -1,14 +1,12 @@
 def is_palindrom(number):
-    if str(number) == str(number) [::-1]:
+    if str(number) == str(number)[::-1]:
         return True
     else:
         return False
 
 foo = 0
-for i in range(1,1000):
-    for j in range(1,1000):
-        if is_palindrom(i*j) and i*j>foo:
+for i in range(1, 1000):
+    for j in range(1, 1000):
+        if is_palindrom(i*j) and i*j > foo:
             foo = i*j
 print(foo)
-
-
